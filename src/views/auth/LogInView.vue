739889388e380d36d904298a69e35e67bfff13bb
Ian Kit Capunong <yayan.cap12@gmail.com>
2025-05-02 22:04:46 +0800

    Add fish product

--- src/views/auth/LogInView.vue
+++ src/views/auth/LogInView.vue
@@ -66,18 +66,17 @@ const onFormSubmit = () => {
               @click:append-inner="isPasswordVisible = !isPasswordVisible"
               :rules="[requiredValidator]"
             ></v-text-field>
-            <router-link to="/Dashboard" class="text-decoration-none">
-              <v-btn
-                class="mt-4"
-                color="green"
-                size="default"
-                variant="tonal"
-                block
-                @click="onFormSubmit"
-              >
-                Log In
-              </v-btn>
-            </router-link>
+
+            <v-btn
+              class="mt-4"
+              color="green"
+              size="default"
+              variant="tonal"
+              block
+              @click="onFormSubmit"
+            >
+              Log In
+            </v-btn>
           </v-form>
 
           <v-card-text class="text-center text">
